b6d7ff98a92a1afb26221224f0e9fe10f6b2e2dc
goodnessidontknow <jackslater1@Gmail.com>
2019-10-14 16:17:36 -0700

    The lidar and colour sensor finally works! Autonomous opmode Sensor was for tests.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/Sensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/Sensor.java
@@ -3,50 +3,39 @@ package org.firstinspires.ftc.teamcode.opmodes.autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-
-import org.firstinspires.ftc.teamcode.components.ColorSystem;
-import org.firstinspires.ftc.teamcode.components.LidarSystem;
+import org.firstinspires.ftc.teamcode.components.color.ColorSystem;
+import org.firstinspires.ftc.teamcode.components.LidarNavigationSystem;
 
 @Autonomous(name = "Sensor")
 public class Sensor extends LinearOpMode {
-    private ColorSensor sensor;
-    private ColorSystem system;
+    public ColorSystem colorSystem;
 
-    private DistanceSensor lidar;
-    private DistanceSensor lidar2;
-    private LidarSystem distance1;
-    private LidarSystem distance2;
+    public LidarNavigationSystem distance1;
 
-    public Sensor(){
-        initialize();
-        system = new ColorSystem(sensor);
-    }
-
-    public void initialize() {
-        sensor = hardwareMap.colorSensor.get("color_sensor");
-        distance1 = new LidarSystem(this, lidar);
-        distance2 = new LidarSystem(this, lidar2);
-    }
-
+    @Override
     public void runOpMode() {
 
+        colorSystem = new ColorSystem(this);
+        distance1 = new LidarNavigationSystem(this);
+
         waitForStart();
+        while (opModeIsActive()) {
+            colorSystem.bLed(false);
 
-        int alpha = sensor.alpha();
-        int red = sensor.red();
-        int green = sensor.green();
-        int blue = sensor.blue();
+            telemetry.addData("Alpha:", colorSystem.getAlpha() / 39);
+            telemetry.addData("Red:", colorSystem.getRed() / 39);
+            telemetry.addData("Green:", colorSystem.getGreen() / 39);
+            telemetry.addData("Blue:", colorSystem.getBlue() / 39);
 
-        telemetry.addData("Alpha:", alpha);
-        telemetry.addData("Red:", red);
-        telemetry.addData("Green:", green);
-        telemetry.addData("Blue:", blue);
+            telemetry.addData("isRed:", colorSystem.isRed());
+            telemetry.addData("isBlue:", colorSystem.isBlue());
+            telemetry.addData("isYellow:", colorSystem.isYellow());
 
-        telemetry.addData("isRed:", system.isRed());
-        telemetry.addData("isBlue:", system.isBlue());
-        telemetry.addData("isYellow:", system.isYellow());
+            telemetry.addData("dist1:", distance1.getDistance1());
+            telemetry.addData("dist2:", distance1.getDistance2());
+
+            telemetry.update();
+        }
     }
 
 }
