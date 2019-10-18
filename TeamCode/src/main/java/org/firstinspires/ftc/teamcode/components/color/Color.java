4cc592f41e86196f196c519cd5780a08233f75c3
thort <44458087+thort1@users.noreply.github.com>
2019-10-17 21:43:10 -0700

    Attempts to make code prettier.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/color/Color.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/color/Color.java
@@ -4,18 +4,18 @@ public class Color {
     public int red;
     public int green;
     public int blue;
-    public final static double tolerance = 10;
+    public final static double TOLERANCE = 10;
+
     public Color(int red, int green, int blue) {
         this.red = red;
         this.green = green;
         this.blue = blue;
     }
     public boolean equals(Color other) {
-        int sum = other.red + other.green + other.blue;
-        return isWithin(this.red / sum, other.red / sum) && isWithin(this.green / sum, other.green / sum) && isWithin(this.blue / sum, other.blue / sum);
+        return isWithin(this.red, other.red) && isWithin(this.blue, other.blue) && isWithin(this.green, other.green);
     }
 
     private boolean isWithin(int a, int b) {
-        return Math.abs(a - b) <= tolerance;
+        return Math.abs(a - b) <= TOLERANCE;
     }
 }
