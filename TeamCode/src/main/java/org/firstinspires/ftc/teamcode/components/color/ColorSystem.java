978032945cd5e626380044e6fadf8b47198df8c8
goodnessidontknow <jackslater1@Gmail.com>
2019-10-22 16:55:19 -0700

    did a git-o commit-o on the lidar system to make it more useful. now, it is useful.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/color/ColorSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/color/ColorSystem.java
@@ -2,9 +2,6 @@ package org.firstinspires.ftc.teamcode.components.color;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.teamcode.components.DriveSystem;
 
 public class ColorSystem {
 
@@ -15,13 +12,9 @@ public class ColorSystem {
     private final Color RED_LINE = new Color(255, 0, 0);//TODO: ADJUST VALUES
     private final Color BLUE_LINE = new Color(0, 0, 255);
 
-    private static final double SCALE_FACTOR = 3.9;
-
     ColorSensor colorSensor;
-    private static DriveSystem driveSystem;
 
     public ColorSystem(OpMode opMode) {
-        //colorSensor = opMode.hardwareMap.get(ColorSensor.class, "color_sensor");
         colorSensor = opMode.hardwareMap.colorSensor.get("color");
     }
 
@@ -58,18 +51,25 @@ public class ColorSystem {
     }
 
     public enum OverLineSettings {
-        OVER_RED (5, 1, 1),
-        OVER_BLUE (1, 1, 5);
+        OVER_RED (3, 6, 0, 2, 0, 2),
+        OVER_BLUE (0, 2, 0, 2, 3,6);
 
-        public int r;
-        public int g;
-        public int b;
+        public int r_min;
+        public int r_max;
+        public int g_min;
+        public int g_max;
+        public int b_min;
+        public int b_max;
 
-        OverLineSettings(int r, int g, int b) {
-            this.r = r;
-            this.g = g;
-            this.b = b;
+        OverLineSettings(int r_min, int r_max, int g_min, int g_max, int b_min, int b_max) {
+            this.r_min = r_min;
+            this.r_max = r_max;
+            this.g_min = g_min;
+            this.g_max = g_max;
+            this.b_min = b_min;
+            this.b_max = b_max;
         }
+
     }
 
     public boolean checkIfOverLine(OverLineSettings toCheck) {
