19bcd1631425071d6d436001bf02e85641a09b2b
thort <44458087+thort1@users.noreply.github.com>
2020-01-12 11:56:28 -0800

    Waiting.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/WaitAuto.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.opmodes.autonomous;
+
+import android.util.Log;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.components.DriveSystem;
+
+@Autonomous(name = "WaitAuto", group="Autonomous")
+public class WaitAuto extends BaseAutonomous {
+    private boolean arrived;
+    private ElapsedTime time;
+
+    // In seconds
+    public static final int WAIT_TIME = 15;
+
+    public void init(Team team) {
+        super.init(team);
+        arrived = false;
+        time = new ElapsedTime();
+    }
+
+    @Override
+    public void loop() {
+        if (!(time.milliseconds() > WAIT_TIME * 1000)) {
+            return;
+        }
+        if (!arrived) {
+            if (driveSystem.driveToPosition(400, DriveSystem.Direction.FORWARD, 1.0)) {
+                arrived = true;
+                return;
+            }
+        }
+    }
+
+}
