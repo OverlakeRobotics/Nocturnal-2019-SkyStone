88b64266d8c48091442da088817c38da34e6266c
Adrian Avram <Adrian_Avram@Outlook.com>
2020-03-02 15:44:13 -0800

    Rolly bot stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/RollyBot.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "RollyBot", group="TeleOp")
+public class RollyBot extends OpMode {
+    DcMotor mainWheel;
+    DcMotor reactionWheel;
+
+    public void init() {
+        mainWheel = hardwareMap.get(DcMotor.class, "mainWheel");
+
+        reactionWheel = hardwareMap.get(DcMotor.class, "reactionWheel");
+
+        mainWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//        flyWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    public void loop() {
+        double mainPower = gamepad1.left_stick_y;
+
+        if (Math.abs(mainPower) < 0.1) {
+            mainPower = 0.0;
+        }
+
+        mainWheel.setPower(mainPower);
+
+        if (gamepad1.right_bumper) {
+            reactionWheel.setPower(1);
+        } else if (gamepad1.left_bumper) {
+            reactionWheel.setPower(-1);
+        } else {
+            reactionWheel.setPower(0);
+        }
+
+
+    }
+}
